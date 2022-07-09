5bf3f9da4fa9ffb06a6de537485ba4266b8f9dc0
q <q@q.q>
2022-07-09 21:59:55 +0300

    add line by line reading

--- main.go
+++ main.go
@@ -1,16 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "io"
+	"bufio"
+	"log"
+	"os"
+)
 
-func init(){
+func init() {
 	fmt.Println("Init")
 }
 
-func main(){
+func main() {
 	displayHello()
+	processFile("./example.txt", func(s string) { fmt.Println(s) })
 }
 
-func displayHello(){
+func processFile(filename string, callback func(string)) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var line string = scanner.Text()
+		callback(line)
+	}
+
+	return nil
+}
+
+func readEntireFile(filename string) string {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(file)
+
+}
+
+func displayHello() {
 	fmt.Println(`  ___           _                        ____
  |_ _|  _ __   | |__     ___   __  __   |  _ \    __ _   _ __   ___    ___   _ __
   | |  | '_ \  | '_ \   / _ \  \ \/ /   | |_) |  / _\ | | '__| / __|  / _ \ | '__|
@@ -18,17 +48,16 @@ func displayHello(){
  |___| |_| |_| |_.__/   \___/  /_/\_\   |_|      \__,_| |_|    |___/  \___| |_|`)
 }
 
-
-func getHello(num int) string{
+func getHello(num int) string {
 	hellos := []string{
 		"Hi, aboba",
-		"Hello, biba" ,
+		"Hello, biba",
 		"Dobryi den, milsdar",
 	}
-	return(hellos[num % (len(hellos))])
+	return (hellos[num%(len(hellos))])
 }
 
-func readLine() string{
+func readLine() string {
 	var str string
 	fmt.Println("Enter string")
 	fmt.Scan(&str)
